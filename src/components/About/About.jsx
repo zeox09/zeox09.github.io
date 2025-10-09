da07d647813db7a7cdf4d60e0b0925fd2779a922
zeox09 <jorgevillan06@gmail.com>
2025-10-09 01:35:17 -0300

    creacion de descripcion y rediseño de componentes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,8 @@ const About = () => {
 return (
 <section id="About" className="h-screen justify-center content-center text-center p-5  items-center ">
     <h1 className="about-text text-red-600 text-5xl">About Me</h1>
-    <p className="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. At aut blanditiis velit illo dolorem nobis odio obcaecati rem facere neque optio sapiente ut libero voluptatibus in id, quis, temporibus pariatur. Illo quod aspernatur quas ipsum quo obcaecati qui mollitia ea explicabo debitis? Dolore doloribus sapiente illo in veniam aspernatur nesciunt animi fuga voluptates amet. Commodi incidunt tempore blanditiis consequatur impedit.</p>
+    <p className="parrafo">
+    </p>
 </section>
 );
 }
--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import Minesweeper from "./Minesweeper";
 
 export default function Game() {
   return (
-    <section
-      id="Game"
-      className="h-screen flex justify-center items-center bg-gray-100 p-5"
-    >
-      <Minesweeper />
+    <section>
     </section>
   );
 }
